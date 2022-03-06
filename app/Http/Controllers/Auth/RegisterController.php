5c64418e8222baaa3e1d98c2a4dedb3b540e59fc
Dawit Mekonnen <dawityitagesu@gmail.com>
2022-03-06 16:00:47 +0300

    validate register and redirect user to dashboard

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+
+class RegisterController extends Controller
+{
+
+    public function index()
+    {
+        return view('auth.register');
+    }
+    public function store(Request $request)
+    {
+        //validate 
+        $attributes = $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|confirmed|min:8',
+        ]);
+        //store
+        $attributes['password'] = bcrypt($attributes['password']); //hash the password
+        User::create($attributes);
+        //sign user in 
+
+        //redirect
+        return redirect()->route('dashboard');
+    }
+}
