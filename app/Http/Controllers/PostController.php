9a3fb544dd3011031d12a1e207690d69e12a5367
Dawit Mekonnen <dawityitagesu@gmail.com>
2022-03-06 20:07:46 +0300

    pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(){
-        $posts=Post::get();
+        $posts=Post::paginate(3);
         return view('posts.index',[
             'posts'=>$posts
         ]);
