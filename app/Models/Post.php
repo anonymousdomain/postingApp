a6f8ba0080b03202c26569f5a79d192e19201bc9
Dawit Mekonnen <dawityitagesu@gmail.com>
2022-03-07 09:10:07 +0300

    liking and unliking

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,12 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    protected $guarded=[];
+    protected $guarded = [];
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
-    public function likes(){
+
+    public function likedBy(User $user){
+
+        return $this->likes->contains('user_id',$user->id);
+    }
+    public function likes()
+    {
         return $this->hasMany(Like::class);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -27,15 +27,20 @@
                             class="text-grey-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
                         <p class="mb-2">{{ $post->body }}</p>
                         <div class="flex items-center">
-                            <form action="{{ route('posts.likes',$post->id) }}" method="POST" class="mr-1">
-                                @csrf
-                                <button type="submit" class="text-blue-500">Like</button>
-                            </form>
-                            <form action="" method="POST" class="mr-1">
-                                @csrf
-                                <button type="submit" class="text-blue-500">Unlike</button>
-                            </form>
-                            <span>{{$post->likes->count()}} {{Str::plural('like',$post->likes->count())}}</span>
+                            @if (!$post->likedBy(auth()->user()))
+                                <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
+                                    @csrf
+                                    <button type="submit" class="text-blue-500">Like</button>
+                                </form>
+                            @else
+                                <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="text-blue-500">Unlike</button>
+                                </form>
+                            @endif
+
+                            <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
                         </div>
                     </div>
                 @endforeach
