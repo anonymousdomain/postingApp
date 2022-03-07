96c7079cc3b4c9a1aaf253b6cc6f41bf26455eba
Dawit Mekonnen <dawityitagesu@gmail.com>
2022-03-07 09:21:21 +0300

    N+1problem solved

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,27 +1,30 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use  App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(){
-        $posts=Post::paginate(15);
-        return view('posts.index',[
-            'posts'=>$posts
+    public function index()
+    {
+        $posts = Post::with(['user', 'likes'])->paginate(15);
+        return view('posts.index', [
+            'posts' => $posts
         ]);
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
 
-        $this->validate($request,[
-            'body'=>'required|min:5'
+        $this->validate($request, [
+            'body' => 'required|min:5'
         ]);
 
         $request->user()->posts()->create([
-            'body'=>$request->body
+            'body' => $request->body
         ]);
-      return back();  
+        return back();
     }
 }
